f043df82c686c7b2ddad8eeef5dc6db4f96c4339
maximiliysiss <zim.ma2010@yandex.ru>
2019-12-20 01:32:45 +0300

    init

---
+++ Helper/BrounMovement/BrounMovement/GravitySystem.cpp
@@ -0,0 +1,28 @@
+#include "GravitySystem.h"
+
+GravitySystem* GravitySystem::gravitySystem = nullptr;
+
+GravitySystem& GravitySystem::getInstance()
+{
+	if (gravitySystem)
+		return *gravitySystem;
+	gravitySystem = new GravitySystem();
+	return *gravitySystem;
+}
+
+void GravitySystem::addObject(SceneObject* obj)
+{
+	sceneObjects.push_back(obj);
+}
+
+void GravitySystem::calculate() const
+{
+	for (unsigned i = 0; i < sceneObjects.size(); i++) {
+		for (unsigned j = i + 1; j < sceneObjects.size(); j++) {
+			if (sceneObjects[i]->isCollision(*sceneObjects[j])) {
+				sceneObjects[i]->collisionAction(*sceneObjects[j]);
+				sceneObjects[j]->collisionAction(*sceneObjects[i]);
+			}
+		}
+	}
+}
