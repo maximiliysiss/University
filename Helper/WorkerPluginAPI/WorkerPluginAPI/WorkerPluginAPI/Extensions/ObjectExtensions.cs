61db4202a0659f786f31b24269055b5d13d34a33
Maxim Zimin <MZimin@egartech.com>
2020-01-17 16:53:20 +0300

    change system date

--- Helper/WorkerPluginAPI/WorkerPluginAPI/WorkerPluginAPI/Extensions/ObjectExtensions.cs
+++ Helper/WorkerPluginAPI/WorkerPluginAPI/WorkerPluginAPI/Extensions/ObjectExtensions.cs
@@ -8,5 +8,6 @@ namespace WorkerPluginAPI.Extensions
     public static class ObjectExtensions
     {
         public static bool In<T>(this T obj, params T[] values) => values.Any(x => x.Equals(obj));
+        public static bool NotIn<T>(this T obj, params T[] values) => !obj.In(values);
     }
 }
