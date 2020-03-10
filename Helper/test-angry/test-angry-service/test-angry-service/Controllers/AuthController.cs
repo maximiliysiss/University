b05b992dd5e61b60ced8b1a9c2c874fb4a1dc1fd
maximiliysiss <zim.ma2010@yandex.ru>
2020-03-10 17:36:04 +0300

    init auth

---
+++ Helper/test-angry/test-angry-service/test-angry-service/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using test_angry_service.Services;
+
+namespace test_angry_service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly DatabaseContext databaseContext;
+
+        public AuthController(DatabaseContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+
+        [Authorize]
+        public ActionResult Login()
+        {
+            return Ok();
+        }
+
+        [Authorize]
+        public ActionResult Register()
+        {
+            return Ok();
+        }
+    }
+}
