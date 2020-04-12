46a3c534d74bf0ea284456ea19df33939fa696fd
Maxim Zimin <zim.ma2010@yandex.ru>
2020-04-12 15:45:28 +0300

    init

---
+++ Helper/TranslateChatter/TranslateChatter/Services/YandexTranslateService.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http;
+
+namespace TranslateChatter.Services
+{
+    public interface ITranslateService
+    {
+        string Translate(string text, string lang);
+    }
+
+    public class TranslateConfiguration
+    {
+        public string Key { get; set; }
+        public string BaseUrl { get; set; }
+    }
+
+
+    public class YandexTranslateService : ITranslateService
+    {
+        private readonly TranslateConfiguration translateConfiguration;
+
+        public YandexTranslateService(TranslateConfiguration translateConfiguration)
+        {
+            this.translateConfiguration = translateConfiguration;
+        }
+
+        public string Translate(string text, string lang)
+        {
+            using HttpClient httpClient = new HttpClient();
+
+            return string.Empty;
+        }
+    }
+}
