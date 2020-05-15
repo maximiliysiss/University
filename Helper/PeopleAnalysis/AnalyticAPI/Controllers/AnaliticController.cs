90d84f71b53584c468ed95da2ffa0088928f99b4
Maxim Zimin <zim.ma2010@yandex.ru>
2020-05-15 09:29:22 +0300

    fix

---
+++ Helper/PeopleAnalysis/AnalyticAPI/Controllers/AnaliticController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PeopleAnalysis.Extensions;
+using PeopleAnalysis.Services;
+using PeopleAnalysis.ViewModels;
+
+namespace PeopleAnalysis.Controllers
+{
+    [Authorize]
+    public class AnaliticController : Controller
+    {
+        private readonly AnaliticService analiticService;
+
+        public AnaliticController(AnaliticService analiticService)
+        {
+            this.analiticService = analiticService;
+        }
+
+        public IActionResult StartAnalys([FromForm]AnalitycsRequestModel analitycsRequest)
+        {
+            if (analiticService.CreateRequest(analitycsRequest, User.UserId()))
+                return RedirectToActionPermanent("Index", "Request");
+            return BadRequest();
+        }
+    }
+}
--- Helper/PeopleAnalysis/AnalyticAPI/Startup.cs
+++ Helper/PeopleAnalysis/AnalyticAPI/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CommonCoreLibrary.Startup;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace AnalyticAPI
 {
@@ -26,6 +20,8 @@ namespace AnalyticAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddJwtAuth(Configuration);
+            services.AddSwagger(Configuration["ApiInfo:Name"], Configuration["ApiInfo:Version"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +37,8 @@ namespace AnalyticAPI
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwaggerWithUI(Configuration["ApiInfo:Name"], Configuration["ApiInfo:Version"]);
 
             app.UseEndpoints(endpoints =>
             {
